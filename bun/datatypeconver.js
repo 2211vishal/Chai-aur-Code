6d84ef3bc94e0fbaca016c331426edfc9ba34511
2211vishal <128148973+2211vishal@users.noreply.github.com>
2024-11-01 07:57:27 +0000

    conversion of datatypes

---
+++ bun/datatypeconver.js
@@ -0,0 +1,34 @@
+let score ="22shm"
+//let score=null // the value of null is zero when null to number conversion
+//let score=undefined // the value of undefined id NaN
+//let score=true // the value of boolean true is 1
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valinnumber=Number(score)
+console.log(typeof valinnumber);
+
+console.log(valinnumber);
+
+
+// "33" => 33 easily convert in to 33
+// "33abc" =>NaN not an number
+// true => 1 // false=> 0
+
+
+let number=1
+let boolean=Boolean(number)
+console.log(boolean);
+
+// true =>1 //false=>0
+// "" => false
+//"hitesh" => true
+
+
+let somenumber=33
+let convertstring= String(somenumber)
+console.log(convertstring);
+console.log(typeof convertstring);
